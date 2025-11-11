85b60eb27aebf586bb68802c0f9298a936e61391
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-11-11 23:15:15 +0530

    fist commit

---
+++ first.js
@@ -0,0 +1,9 @@
+
+10+20
+const a = 6;
+a*5
+const fs =require('fs')
+fs.writeFileSync('test.txt','hello world!')
+fs.readFileSync('test.txt','utf8')
+fs.readFileSync('test.txt','hex')
+fs.readFileSync('test.txt','utf8')

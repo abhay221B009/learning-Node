971d0b29d56d54893706b6741c3a0083957d4f47
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-11-14 11:17:34 +0530

    dns and webWrokFlow

--- first.js
+++ first.js
@@ -1,9 +1,15 @@
-
-10+20
+10 + 20;
 const a = 6;
-a*5
-const fs =require('fs')
-fs.writeFileSync('test.txt','hello world!')
-fs.readFileSync('test.txt','utf8')
-fs.readFileSync('test.txt','hex')
-fs.readFileSync('test.txt','utf8')
+a * 5;
+const fs = require("fs");
+fs.writeFileSync("test.txt", "hello world!");
+fs.readFileSync("test.txt", "utf8");
+fs.readFileSync("test.txt", "hex");
+fs.readFileSync("test.txt", "utf8");
+fs.writeFile("test3.txt", "this is test number 3", (err) => {
+  if (err) throw err;
+});
+
+fs.readFile("test3.txt", "utf8", (err, data) => {
+  console.log(data);
+});

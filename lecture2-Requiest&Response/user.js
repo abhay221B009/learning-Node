8a1cb11e1480aabfea0f57edcb63a1d2f0d1038d
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-11-19 00:25:14 +0530

    learning about taking user input and responding

--- lecture2-Requiest&Response/user.js
+++ lecture2-Requiest&Response/user.js
@@ -2,33 +2,41 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+
   if (req.url === "/") {
-    //sending response back to client
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>home</title></head>");
-    res.write("<body><h1>Welcome to home</h1></body>");
-    res.write("</html>");
-    // res.end();
-    // return;
-    return res.end();
-  } else if (req.url === "/products") {
-    //sending response back to client
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My first Page</title></head>");
-    res.write("<body><h1>You are in products section</h1></body>");
+    res.write("<body><h1>Welcome to home</h1>");
+
+    // Form with correct labels and radio buttons
+    res.write("<form>");
+    res.write('<input type="text" name="username" placeholder="name"><br><br>');
+
+    res.write('<input type="radio" id="male" name="gender" value="male">');
+    res.write('<label for="male"> Male </label><br>');
+
+    res.write('<input type="radio" id="female" name="gender" value="female">');
+    res.write('<label for="female"> Female </label><br><br>');
+
+    res.write('<button type="submit">Submit</button>');
+    res.write("</form>");
+
+    res.write("</body>");
     res.write("</html>");
     return res.end();
   } else {
-    //sending response back to client
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>My first Page</title></head>");
-    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("<body><h1>Enter your details</h1></body>");
     res.write("</html>");
-    res.end();
+    return res.end();
   }
+});
 
-  // process.exit();
+const port = 3001;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
